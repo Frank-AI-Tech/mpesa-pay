4d78e7e8c284179d51a9750355e008fe62a825ce
Confronter <confronterbackup@gmail.com>
2025-07-01 17:23:59 +0300

    🔥

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <div className="header">
       <div className="logo-container">
-        <img src="https://i.ibb.co/DJybLFx/lordcasey.jpg" alt="CASEYRHODES TECH LOGO" />
+        <img src="https://files.catbox.moe/d8z3qs.PNG" alt="CONFRONTER TECH LOGO" />
       </div>
       <h3>M-PESA CHECKOUT</h3>
     </div>
